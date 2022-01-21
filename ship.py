253e5af2d32af459aa35326dfe2d69815e063540
1000-7??? <arch@ar4.localdomain>
2022-01-22 00:11:48 +0300

    ÐСДоделал полностью пÑодинарные и двойные кораблики, тесты тож

--- ship.py
+++ ship.py
@@ -1,81 +1,166 @@
 #
-# Основной класс игрока с хранением координат кораблей и их статусов
-#
-class player:
-
-    # Атрибуты, хранящие в себе все данные о кораблях
-    _first_1x_ship = None
-    _second_1x_ship = None
-    _third_1x_ship = None
-    _fourth_1x_ship = None
-    _first_2x_ship = None
-    _second_2x_ship = None
-    _third_2x_ship = None
-    _first_3x_ship = None
-    _second_3x_ship = None
-    _single_4x_ship = None
-
-    # Пока что пустой инит, мб так и останеться пустым...не знаю
-    def __init__(self):
-        pass
-
-    #
-    # Сеттеры для одинарных кораблей
-    #
-    def set_first_1x_ship(self,cord):
-        self._first_1x_ship = ship_1x(cord)
-
-    def set_second_1x_ship(self,cord):
-        self._second_1x_ship = ship_1x(cord)
-    
-    def set_third_1x_ship(self, cord):
-        self._third_1x_ship = ship_1x(cord)
-    
-    def set_fourth_1x_ship(self, cord):
-        self._fourth_1x_ship = ship_1x(cord)
-        
-    #
-    # Геттеры для одинарных кораблей
-    #
-    def get_first_1x_ship(self):
-        return self._first_1x_ship
-
-    def get_second_1x_ship(self):
-        return self._second_1x_ship
-
-    def get_third_1x_ship(self):
-        return self._third_1x_ship
-
-    def get_fourth_1x_ship(self):
-        return self._fourth_1x_ship
-
-#
 # Основной класс кораблей, в котором прописана логика существования корабля
 #
 class ship:
 
     # Атрибут, который показывает мертв ли кораблик
     _status = None
+    #Координаты корабля
+    _xy_cord = None
+    # Клетки вокруг корабля
+    _cells_around_ship = None
+
+    _dead_cells = None
 
     # Инит, пока что только присваивает кораблю статус "живой"
     def __init__(self):
-        self.set_status(True)
+        self.status = True
+        self._xy_cord = []
+        self._cells_around_ship = []
+        self._dead_cells = []
 
-    # Сеттер для статуса, лучше не ошибайся, а то словишь исключение
-    def set_status(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("_status can be only bool")
+    # Геттер для координат
+    @property
+    def xy_cord(self):
+        return self._xy_cord
+
+    @xy_cord.setter
+    def xy_cord(self, xy):
+        # Проверка xy на тип
+        if not isinstance(xy, tuple):
+            raise TypeError("xy must be tuple")
+        # Проверка xy на длину
+        elif len(xy) != 2:
+            raise ValueError("xy must have only 2 args")
+        # Проверка x на тип
+        elif not isinstance(xy[0], int):
+            raise TypeError("x must be int")
+        # Проверка y на тип
+        elif not isinstance(xy[1], int):
+            raise TypeError("y must be int")
+        # Проверка x на принадлежность к [0-9]
+        elif xy[0] < 0 or xy[0] > 9:
+            raise ValueError("x must be in range [0-9]")
+        # Проверка y на принадлежность к [0-9]
+        elif xy[1] < 0 or xy[1] > 9:
+            raise ValueError("y must be in range [0-9]")
+        # Добавление координаты в массив
         else:
-            self._status = value
-    
+            self._xy_cord.append((xy[0],xy[1]))
+
+    # Геттер массива клеток вокруг корабля
+    @property
+    def cells_around_ship(self):
+        return self._cells_around_ship
+
+
+    # Сеттер массива клеток вокруг корабля
+    @cells_around_ship.setter
+    def cells_around_ship(self, xy):
+        # Проверка типа xy
+        if not isinstance(xy, tuple):
+            raise TypeError("xy must be tuple")
+        # Проверка длины xy
+        elif len(xy) != 2:
+            raise ValueError("xy must have only 2 values")
+        # Проверка типа x
+        elif not isinstance(xy[0], int):
+            raise TypeError("x must be int")
+        # Проверка принадлежности x к [0-9]
+        elif xy[0] < 0 or xy[0] > 9:
+            raise ValueError("x must be in range [0-9]")
+        # Проверка типа y
+        elif not isinstance(xy[1], int):
+            raise TypeError("y must be int")
+        # Проверка принадлежности y к [0-9]
+        elif xy[1] < 0 or xy[1] > 9:
+            raise ValueError("y must be in range [0-9]")
+        # Добавление xy в массив cells_arround_ship
+        else:
+            self._cells_around_ship.append(xy)
+
+    #Геттер массива мертвых клеток
+    @property
+    def dead_cells(self):
+        return self._dead_cells
+
+    #Сеттер массива мертвых клеток
+    @dead_cells.setter
+    def dead_cells(self, xy):
+        # Проверка типа xy
+        if not isinstance(xy, tuple):
+            raise TypeError("xy must be tuple")
+        # Проверка длины xy
+        elif len(xy) != 2:
+            raise ValueError("xy must have only 2 values")
+        # Проверка типа x
+        elif not isinstance(xy[0], int):
+            raise TypeError("x must be int")
+        # Проверка принадлежности x к [0-9]
+        elif xy[0] < 0 or xy[0] > 9:
+            raise ValueError("x must be in range [0-9]")
+        # Проверка типа y
+        elif not isinstance(xy[1], int):
+            raise TypeError("y must be int")
+        # Проверка принадлежности y к [0-9]
+        elif xy[1] < 0 or xy[1] > 9:
+            raise ValueError("y must be in range [0-9]")
+        # Добавление xy в массив cells_arround_ship
+        else:
+            self._dead_cells.append(xy)
+
     # Геттер для статуса
-    def get_status(self):
+    @property
+    def status(self):
         return self._status
 
+    # Сеттер для статуса, лучше не ошибайся, а то словишь исключение
+    @status.setter
+    def status(self, value):
+        # Проверка value на тип 
+        if not isinstance(value, bool):
+            raise TypeError("_status can be only bool")
+        # Запись value в status
+        else:
+            self._status = value
+
+    # Обработчик удара
+    def hit(self, cord):
+        # Проверка типа cord
+        if not isinstance(cord, str):
+            raise TypeError("hitting cord must be str")
+        # Проверка длины cord
+        elif len(cord) != 2:
+            raise ValueError("hitting cord must have only 2 symbol")
+        # Проверка первого символа на значения [a-j]
+        elif ship.char_to_int(cord[0].lower()) == 666:
+            raise ValueError("first symbol must be in range [a-j]")
+        # Проверка второго символа cord на возможность представить в типе int
+        elif not ship.convert_to_int(cord[1]):
+            raise TypeError("second symbol must be converted to int")
+        # Проверка попадания по кораблю возвращает True при попадании и False при промахе
+        else:
+            hit_x = ship.char_to_int(cord[0].lower())
+            hit_y = int(cord[1])
+            item = (hit_x, hit_y)
+            if item in self.xy_cord:
+                self.dead_cells = (hit_x, hit_y)
+                for item in self.xy_cord:
+                    if item not in self.dead_cells:
+                        self.create_unuseable_cells(hit_x, hit_y)
+                        return True
+                self.status = False
+                self.create_unuseable_cells(hit_x, hit_y)
+                return True
+            else:
+                return False
+
+        
     #
     # Ваууу, в питоне 3.10 завезли матч кейс
     # Функция, которая преобразовывает буквенную координату в числовую
     #
+    @staticmethod
     def char_to_int(char):
         match char:
             case "a":
@@ -101,6 +186,10 @@ class ship:
             case _:
                 return 666
     
+    #
+    # Функция проверяющая возможность конвертнуть char в int
+    #
+    @staticmethod
     def convert_to_int(symbol):
         try:
             int(symbol)
@@ -112,11 +201,6 @@ class ship:
 # Класс одинарных кораблей
 #
 class ship_1x(ship):
-
-    # Атрибуты координат x и y, всего две так как это одинарный корабль
-    # Важно: только int
-    _x_cord = None
-    _y_cord = None
 
     # Инит, присваивает координаты кораблику с переменной cord которая имеет тип str
     def __init__(self, cord):
@@ -135,44 +219,187 @@ class ship_1x(ship):
             raise TypeError("second symbol must be converted to int")
         # Создание объекта
         else:
-            self.set_x_cord(ship.char_to_int(cord[0].lower()))
-            self.set_y_cord(int(cord[1]))
             ship.__init__(self)
+            self.xy_cord = (ship.char_to_int(cord[0].lower()), int(cord[1]))
 
-    # Геттеры для координат, только int и не больше 9
-    def set_x_cord(self, value):
-        if not isinstance(value, int):
-            raise TypeError("_x_cord can be only int")
-        elif value > 9 or value < 0:
-            raise ValueError("_x_cord can only be in range [0-9]")
+    
+    # Заполнение массива клеток вокруг корабля
+    def create_unuseable_cells(self, x, y):
+
+        if not isinstance(x, int):
+            raise TypeError("x must be int")
+        elif x > 9 or x < 0:
+            raise ValueError("x must be in range [0-9]")
+        elif not isinstance(y, int):
+            raise TypeError("y must be int")
+        elif y > 9 or y < 0:
+            raise ValueError("y must be in range [0-9]")
+
+        for i in range(x-1, x+2):
+            if i < 0 or i > 9:
+                continue
+            for j in range(y-1, y+2):
+                if j < 0 or j > 9 or (i == x and j == y):
+                    continue
+                else:
+                    self.cells_around_ship = (i,j)
+
+#
+# Класс двойных кораблей
+#
+class ship_2x(ship):
+
+    # Инит, присваивает координаты кораблику с переменной cord которая имеет тип str
+    def __init__(self, cord):
+        # Проверка cord на тип str
+        if not isinstance(cord, str):
+            raise TypeError("cord can be only str")
+        # Проверка cord на длину
+        elif len(cord) != 5:
+            raise ValueError("cord lenght always must be 5")
+        # Проверка первого символа на значения [a-j]
+        elif ship.char_to_int(cord[0].lower()) == 666:
+            raise ValueError("first symbol must be in range [a-j]")
+        # Проверка второго символа cord на возможность представить в типе int
+        elif not ship.convert_to_int(cord[1]):
+            raise TypeError("second symbol must be converted to int")
+        # Проверка третьего символа на -
+        elif cord[2] != "-":
+            raise ValueError("third symbol must be -")
+        # Проверка четвертого символа на значения [a-j]
+        elif ship.char_to_int(cord[3].lower()) == 666:
+            raise ValueError("fourth symbol must be in range [a-j]")
+        # Проверка пятого символа cord на возможность представить в типе int
+        elif not ship.convert_to_int(cord[4]):
+            raise TypeError("fifth symbol must be converted to int")
+
+        # Проверка допустимости создания корабля в виде полоски
+        if abs(ship.char_to_int(cord[0].lower()) - ship.char_to_int(cord[3].lower())) == 1:
+            a = True
         else:
-            self._x_cord = value
+            a = False
+
+        if abs(int(cord[1]) - int(cord[4])) == 1:
+            b = True
+        else:
+            b = False
+
+        if (a == b) :
+            raise ValueError("cord must be with length or width equal 2, and sceond param equal 1")
+
+        # Создание объекта
+        else:
+            ship.__init__(self)
+            start_x = ship.char_to_int(cord[0].lower())
+            start_y = int(cord[1])
+            end_x = ship.char_to_int(cord[3].lower())
+            end_y = int(cord[4])
+
+            if start_x > end_x - 1:
+                start_x, end_x = end_x, start_x
+            if start_y > end_y - 1:
+                start_y, end_y = end_y, start_y
+
+            for i in range (start_x, end_x + 1):
+                for j in range(start_y, end_y + 1):
+                    self.xy_cord = (i, j)
+
+    # Заполнение массива клеток вокруг корабля
+    def create_unuseable_cells(self, x, y):
+
+        if not isinstance(x, int):
+            raise TypeError("x must be int")
+        elif x > 9 or x < 0:
+            raise ValueError("x must be in range [0-9]")
+        elif not isinstance(y, int):
+            raise TypeError("y must be int")
+        elif y > 9 or y < 0:
+            raise ValueError("y must be in range [0-9]")
+
+        for i in range(x-1, x+2):
+            if i < 0 or i > 9:
+                continue
+            for j in range(y-1, y+2):
+                if j < 0 or j > 9 or (i,j) in self.xy_cord or (i,j) in self.cells_around_ship:
+                    continue
+                else:
+                    self.cells_around_ship = (i,j)
             
-    def set_y_cord(self, value):
-        if not isinstance(value, int):
-            raise TypeError("_y_cord can be only int")
-        elif value > 9 or value < 0:
-            raise ValueError("_y_cord can only be in range [0-9]")
-        else:
-            self._y_cord = value
-    
-    # Геттеры для координат
-    def get_x_cord(self):
-        return self._x_cord
-    
-    def get_y_cord(self):
-        return self._y_cord
 
-    # Метод чисто так подебажить, больше он не нужен
-    def show_cord(self):
-        print(f"x = {self._x_cord}  y = {self._y_cord}")
+
+#
+# Основной класс игрока с хранением координат кораблей и их статусов
+#
+class player:
+
+    # Атрибуты, хранящие в себе все данные о кораблях
+    _first_1x_ship = None
+    _second_1x_ship = None
+    _third_1x_ship = None
+    _fourth_1x_ship = None
+    _first_2x_ship = None
+    _second_2x_ship = None
+    _third_2x_ship = None
+    _first_3x_ship = None
+    _second_3x_ship = None
+    _single_4x_ship = None
+
+    # Массивы клеток
+
+
+    # Пока что пустой инит, мб так и останеться пустым...не знаю
+    def __init__(self):
+        pass
+
+    #
+    # Геттеры для одинарных кораблей
+    #
+    @property
+    def first_1x_ship(self):
+        return self._first_1x_ship
+
+    @property
+    def second_1x_ship(self):
+        return self._second_1x_ship
+
+    @property
+    def third_1x_ship(self):
+        return self._third_1x_ship
+
+    @property
+    def fourth_1x_ship(self):
+        return self._fourth_1x_ship
+
+    #
+    # Сеттеры для одинарных кораблей
+    #
+    @first_1x_ship.setter
+    def first_1x_ship(self,cord):
+        self._first_1x_ship = ship_1x(cord)
+
+    @second_1x_ship.setter
+    def second_1x_ship(self,cord):
+        self._second_1x_ship = ship_1x(cord)
+    
+    @third_1x_ship.setter
+    def third_1x_ship(self, cord):
+        self._third_1x_ship = ship_1x(cord)
+    
+    @fourth_1x_ship.setter
+    def fourth_1x_ship(self, cord):
+        self._fourth_1x_ship = ship_1x(cord)
+        
+
 
 #
 # Тесты
 #
 def main():
-    oj = player()
-    oj.set_first_1x_ship("b2")
+    obj = ship_2x("c3-c4")
+    obj.hit("c3")
+    print(obj.cells_around_ship)
+    obj.hit("c4")
+    print(obj.cells_around_ship)
 
 if __name__ == "__main__":
     main()
--- test_ship.py
+++ test_ship.py
@@ -1,37 +1,100 @@
 import unittest
-from ship import player, ship, ship_1x
-
-class test_player(unittest.TestCase):
-    
-    #
-    # Создание обхекта для тестирования
-    #
-    def setUp(self):
-
-        self.test_player = player()
-
-    #
-    # Проверка на правильность вывода кораблей 1x
-    #
-    def test_create_ship_1x(self):
-
-        # Проверка правильности вывода первого 1x корабля
-        self.test_player.set_first_1x_ship("a0")
-        self.assertEqual(self.test_player.get_first_1x_ship(), self.test_player._first_1x_ship)
-
-        # Проверка правильности вывода второго 1x корабля
-        self.test_player.set_second_1x_ship("b1")
-        self.assertEqual(self.test_player.get_second_1x_ship(), self.test_player._second_1x_ship)
-
-        # Проверка правильности вывода третьего 1x корабля
-        self.test_player.set_third_1x_ship("c2")
-        self.assertEqual(self.test_player.get_third_1x_ship(), self.test_player._third_1x_ship)
-
-        # Проверка правильности вывода четвертого 1x корабля
-        self.test_player.set_fourth_1x_ship("d3")
-        self.assertEqual(self.test_player.get_fourth_1x_ship(), self.test_player._fourth_1x_ship)
+from ship import player, ship, ship_1x, ship_2x
+from change_color.main import green_color, bald_text, default_text
 
 class test_ship(unittest.TestCase):
+
+    #
+    # Проверка status
+    #
+    def test_status(self):
+        # Проверка на тип
+        with self.assertRaises(TypeError):
+            ship().status = "c/vrs/xd"
+        # Проверка на правильность отображения
+        self.assertEqual(ship().status, True)
+
+    #
+    # Проверка xy_cord
+    #
+    def test_xy_cord(self):
+        # Проверка xy на тип
+        with self.assertRaises(TypeError):
+            ship().xy_cord = [1,2]
+            ship().xy_cord = 1
+        # Проверка xy на длину
+        with self.assertRaises(ValueError):
+            ship().xy_cord = (1,2,3)
+
+        # Проверка x на тип
+        with self.assertRaises(TypeError):
+            ship().xy_cord = ("c/vrs/xd", 2)
+        # Проверка x на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().xy_cord = (-1,2)
+
+        # Проверка y на тип
+        with self.assertRaises(TypeError):
+            ship().xy_cord = (1 , "c/vrs/xd")
+        # Проверка y на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().xy_cord = (1,10)
+
+    #
+    # Проверка клеток вокруг корабля
+    #
+    def test_cells_around_ship(self):
+
+        # Проверка xy на тип
+        with self.assertRaises(TypeError):
+            ship().cells_around_ship = [1,2]
+            ship().cells_around_ship = 1
+
+        # Проверка xy на количество значений
+        with self.assertRaises(ValueError):
+            ship().cells_around_ship = (1,2,3)
+
+        # Проверка x на тип
+        with self.assertRaises(TypeError):
+            ship().cells_around_ship = ("c/vrs/xd", 2)
+        # Проверка y на тип
+        with self.assertRaises(TypeError):
+            ship().cells_around_ship = (1, "c/vrs/xd")
+
+        # Проверка x на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().cells_around_ship = (10, 1)
+        # Проверка y на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().cells_around_ship = (1, -1)
+
+    #
+    # Проверка мертвых клеток
+    #
+    def test_cells_around_ship(self):
+
+        # Проверка xy на тип
+        with self.assertRaises(TypeError):
+            ship().dead_cells = [1,2]
+            ship().dead_cells = 1
+
+        # Проверка xy на количество значений
+        with self.assertRaises(ValueError):
+            ship().dead_cells = (1,2,3)
+
+        # Проверка x на тип
+        with self.assertRaises(TypeError):
+            ship().dead_cells = ("c/vrs/xd", 2)
+        # Проверка y на тип
+        with self.assertRaises(TypeError):
+            ship().dead_cells = (1, "c/vrs/xd")
+
+        # Проверка x на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().dead_cells = (10, 1)
+        # Проверка y на принадлежность к [0-9]
+        with self.assertRaises(ValueError):
+            ship().dead_cells = (1, -1)
 
     #
     # Проверка функции преобразования буквенной координаты в символьную
@@ -53,45 +116,199 @@ class test_ship(unittest.TestCase):
         # Проверка при вводе строки, которую нельзя представить в виде int
         self.assertEqual(ship.convert_to_int("c/vrs/xd"), False)
 
+
 class test_ship_1x(unittest.TestCase):
 
     #
-    # Проверка взаимодействия с координатами
+    # Проверка создания обхекта
     #
-    def test_cord(self):
+    def test_create(self):
 
-        # Проверка get_x
-        self.assertEqual(ship_1x("b2").get_x_cord(), 1)
-        # Проверка get_y
-        self.assertEqual(ship_1x("b2").get_y_cord(), 2)
-        # Проверка на вызов исключения объекта с неверным типом аргумента cord
-        self.assertRaises(TypeError, ship_1x, 1)
-        # Проверка на вызов исключения объекта с неверной длиной аргумента cord
-        self.assertRaises(ValueError, ship_1x, "c/vrs/xd")
-        # Проверка на вызов исключния объекта с неверным первым символом строки cord
-        self.assertRaises(ValueError, ship_1x, "17")
-        # Проверка на вызов исключения объекта с неверным типом второго символа строки cord
-        self.assertRaises(TypeError, ship_1x, "aa")
+        # Проверка на ошибочный тип cord
+        with self.assertRaises(TypeError):
+            ship_1x(1)
+        # Проверка на длину cord
+        with self.assertRaises(ValueError):
+            ship_1x("c/vrs/xd")
+        # Проверка x на принадлежность к [a-j]
+        with self.assertRaises(ValueError):
+            ship_1x("t1")
+        # Проверка y на возможность конвертации в int
+        with self.assertRaises(TypeError):
+            ship_1x("at")
 
-        # Проверка set_x
-        self.assertRaises(TypeError, ship_1x("b2").set_x_cord, "c/vrs/xd") # Проверка на верный тип аргумента
-        self.assertRaises(ValueError, ship_1x("b2").set_x_cord, -1) # Проверка на ввод аргумента <0
-        self.assertRaises(ValueError, ship_1x("b2").set_x_cord, 10) # Проверка на ввод аргумента >9
-
-        # Проверка set_y
-        self.assertRaises(TypeError, ship_1x("b2").set_y_cord, "c/vrs/xd") # Проверка на верный тип аргумента
-        self.assertRaises(ValueError, ship_1x("b2").set_y_cord, -1) # Проверка на ввод аргумента <0
-        self.assertRaises(ValueError, ship_1x("b2").set_y_cord, 10) # Проверка на ввод аргумента >9
-    
     #
     # Проверка взаимодействия с статусом
     #
     def test_status(self):
 
         # Проверка get_status
-        self.assertEqual(ship_1x("b2").get_status(), True)
+        self.assertEqual(ship_1x("b2").status, True)
         # Проверка set_status
-        self.assertRaises(TypeError, ship_1x("b2").set_status, "c/vrs/xd") # Проверка на тип аргумента
+        with self.assertRaises(TypeError):
+            ship_1x("b2").status = "c/vrs/xd"
+
+
+
+    #
+    # Проверка создания массива клеток вокруг корабля
+    #
+    def test_create_unuseable_cells(self):
+
+        # Проверка x на тип
+        self.assertRaises(TypeError, ship_1x("b2").create_unuseable_cells, "c/vrs/xd", 2)
+        # Проверка y на тип
+        self.assertRaises(TypeError, ship_1x("b2").create_unuseable_cells, 1, "c/vrs/xd")
+
+        # Проверка функции на правильность вывода
+        obj = ship_1x("a0")
+        obj.hit("a0")
+        self.assertEqual(obj.cells_around_ship, [(0,1),(1,0),(1,1)])
+
+        obj = ship_1x("j9")
+        obj.hit("j9")
+        self.assertEqual(obj.cells_around_ship, [(8,8),(8,9),(9,8)])
+
+        obj = ship_1x("c3")
+        obj.hit("c3")
+        self.assertEqual(obj.cells_around_ship, [(1,2),(1,3),(1,4),(2,2),(2,4),(3,2),(3,3),(3,4)])
+
+        obj = ship_1x("c3")
+        obj.hit("b2")
+        self.assertEqual(obj.cells_around_ship, [])
+
+    #
+    # Проверка удара по кораблю
+    #
+    def test_hit(self):
+
+        # Проверка на верный тип аргумента
+        self.assertRaises(TypeError, ship_1x("b2").hit, 1)
+        # Проверка на длину аргумента
+        self.assertRaises(ValueError, ship_1x("b2").hit, "c/vrs/xd")
+        # Проверка первого символа на принадлежность к [a-j]
+        self.assertRaises(ValueError, ship_1x("b2").hit, "l1")
+        # Проверка второго символа на возможность преобразовать в int
+        self.assertRaises(TypeError, ship_1x("b2").hit, "ak")
+
+        # Проверка возвращаемого значения после попадания
+        self.assertEqual(ship_1x("b2").hit("b2"), True)
+        # Проверка возвращаемого значения после промаха
+        self.assertEqual(ship_1x("b2").hit("b3"), False)
+
+        # Проверка статуса корабля после попадания
+        obj = ship_1x("b2")
+        obj.hit("b2")
+        self.assertEqual(obj.status, False)
+        # Проверка статуса корабля после промаха
+        obj = ship_1x("b2")
+        obj.hit("b3")
+        self.assertEqual(obj.status, True)
+
+
+class test_ship_2x(unittest.TestCase):
+
+    #
+    # Проверка создания обхекта
+    #
+    def test_create(self):
+
+        # Проверка на ошибочный тип cord
+        with self.assertRaises(TypeError):
+            ship_2x(1)
+        # Проверка на длину cord
+        with self.assertRaises(ValueError):
+            ship_2x("c/vrs/xd")
+        # Проверка x_start на принадлежность к [a-j]
+        with self.assertRaises(ValueError):
+            ship_2x("t1-a1")
+        # Проверка y_start на возможность конвертации в int
+        with self.assertRaises(TypeError):
+            ship_2x("at-a1")
+        # Проверка 3 символа cord на -
+        with self.assertRaises(ValueError):
+            ship_2x("a0@a1")
+        # Проверка x_end на принадлежность к [a-j]
+        with self.assertRaises(ValueError):
+            ship_2x("a1-t1")
+        # Проверка y_end на возможность конвертации в int
+        with self.assertRaises(TypeError):
+            ship_2x("a1-at")
+        # Проверка на невозможность создания корабля 1x1
+        with self.assertRaises(ValueError):
+            ship_2x("a0-a0")
+        # Проверка на невозможность создания корабля Nx1 где N больше 2х
+        with self.assertRaises(ValueError):
+            ship_2x("c0-a0")
+            ship_2x("a0-c0")
+        # Проверка на невозможность создания корабля 1xM где M больше 2х
+        with self.assertRaises(ValueError):
+            ship_2x("a2-a0")
+            ship_2x("a0-a2")
+
+    #
+    # Проверка удара по кораблю
+    #
+    def test_hit(self):
+
+        # Проверка на верный тип аргумента
+        self.assertRaises(TypeError, ship_2x("b2-b1").hit, 1)
+        # Проверка на длину аргумента
+        self.assertRaises(ValueError, ship_2x("b2-b1").hit, "c/vrs/xd")
+        # Проверка первого символа на принадлежность к [a-j]
+        self.assertRaises(ValueError, ship_2x("b2-b1").hit, "l1")
+        # Проверка второго символа на возможность преобразовать в int
+        self.assertRaises(TypeError, ship_2x("b2-b1").hit, "ak")
+
+        # Проверка возвращаемого значения после попадания
+        self.assertEqual(ship_2x("b2-b1").hit("b2"), True)
+        # Проверка возвращаемого значения после промаха
+        self.assertEqual(ship_2x("b2-b1").hit("b3"), False)
+
+        # Проверка статуса корабля после попадания
+        obj = ship_2x("b2-b1")
+        obj.hit("b2")
+        self.assertEqual(obj.status, True)
+        obj.hit("b1")
+        self.assertEqual(obj.status, False)
+        # Проверка статуса корабля после промаха
+        obj = ship_2x("b2-b1")
+        obj.hit("b3")
+        self.assertEqual(obj.status, True)
+
+    #
+    # Проверка создания массива клеток вокруг корабля
+    #
+    def test_create_unuseable_cells(self):
+
+        # Проверка x на тип
+        self.assertRaises(TypeError, ship_2x("b2-a2").create_unuseable_cells, "c/vrs/xd", 2)
+        # Проверка y на тип
+        self.assertRaises(TypeError, ship_2x("b2-a2").create_unuseable_cells, 1, "c/vrs/xd")
+
+        # Проверка функции на правильность вывода
+        obj = ship_2x("a0-b0")
+        obj.hit("a0")
+        self.assertEqual(obj.cells_around_ship, [(0,1),(1,1)])
+        obj.hit("b0")
+        self.assertEqual(obj.cells_around_ship, [(0,1),(1,1),(2,0),(2,1)])
+
+        obj = ship_2x("j9-i9")
+        obj.hit("j9")
+        self.assertEqual(obj.cells_around_ship, [(8,8),(9,8)])
+        obj.hit("i9")
+        self.assertEqual(obj.cells_around_ship, [(8,8),(9,8),(7,8),(7,9)])
+
+        obj = ship_2x("c3-c4")
+        obj.hit("c3")
+        self.assertEqual(obj.cells_around_ship, [(1,2),(1,3),(1,4),(2,2),(3,2),(3,3),(3,4)])
+        obj.hit("c4")
+        self.assertEqual(obj.cells_around_ship, [(1,2),(1,3),(1,4),(2,2),(3,2),(3,3),(3,4),(1,5),(2,5),(3,5)])
+
+        obj = ship_2x("c3-c4")
+        obj.hit("b2")
+        self.assertEqual(obj.cells_around_ship, [])
+
 
 
 if __name__ == "__main__":
